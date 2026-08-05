80964c6dd82d56b28e4d0c54ab6b05fcd252dbef
Daivakrupa Thota <80198240+daivakrupa@users.noreply.github.com>
2026-08-05 12:00:54 +0530

    Update review.py

--- scripts/review.py
+++ scripts/review.py
@@ -1,3 +1,35 @@
+# from google import genai
+# import os
+
+# client = genai.Client(
+#     api_key=os.environ["GEMINI_API_KEY"]
+# )
+
+# with open(".github/workflows/deploy.yml") as f:
+#     pipeline = f.read()
+
+# prompt = f"""
+# Review this CI/CD pipeline.
+
+# Check:
+# 1. Syntax issues
+# 2. Invalid keywords
+# 3. YAML formatting
+
+# Return PASS or FAIL.
+
+# Pipeline:
+
+# {pipeline}
+# """
+
+# response = client.models.generate_content(
+#     model="gemini-1.5-flash",
+#     contents=prompt
+# )
+
+# print(response.text)
+
 from google import genai
 import os
 
@@ -5,27 +37,5 @@ client = genai.Client(
     api_key=os.environ["GEMINI_API_KEY"]
 )
 
-with open(".github/workflows/deploy.yml") as f:
-    pipeline = f.read()
-
-prompt = f"""
-Review this CI/CD pipeline.
-
-Check:
-1. Syntax issues
-2. Invalid keywords
-3. YAML formatting
-
-Return PASS or FAIL.
-
-Pipeline:
-
-{pipeline}
-"""
-
-response = client.models.generate_content(
-    model="gemini-1.5-flash",
-    contents=prompt
-)
-
-print(response.text)
+for model in client.models.list():
+    print(model.name)
